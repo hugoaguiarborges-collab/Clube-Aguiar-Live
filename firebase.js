8807acf35bfeec3fe861b69c2e87c06308800f9e
hugoaguiarborges-collab <hugoaguiarborges@gmail.com>
2025-08-21 22:11:40 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,24 +1,25 @@
-// firebase-init.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
-import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+// Importando funções necessárias do Firebase
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
-// Site estático NÃO lê process.env. Use valores literais do seu Firebase:
+// Configuração do Firebase do seu projeto
 const firebaseConfig = {
   apiKey: "AIzaSyAMf0Z5_a1DufNRCTHGPGC6ZBE9T2-tYAU",
   authDomain: "clube-aguiar-live.firebaseapp.com",
   projectId: "clube-aguiar-live",
-  // bucket padrão do Firebase Storage:
   storageBucket: "clube-aguiar-live.appspot.com",
   messagingSenderId: "694025911850",
   appId: "1:694025911850:web:5f0c6117dbc60b15118d53"
 };
 
+// Inicializando o Firebase
 const app = initializeApp(firebaseConfig);
 
-// Autenticação anônima para funcionar com as regras
-const auth = getAuth(app);
-signInAnonymously(auth).catch(console.error);
+// Exportando serviços para usar em outros arquivos
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
 
-// Deixa o app disponível para outros scripts
-window.__firebaseApp = app;
-
+export default app;
